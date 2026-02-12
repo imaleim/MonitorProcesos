1e034992f684f921384555b10f8f9479c148345b
imaleim <163207689+imaleim@users.noreply.github.com>
2026-02-11 21:39:26 -0600

    Add files via upload

---
+++ Procesos.cpp
@@ -0,0 +1,42 @@
+//Alejandra Itzel Ojeda Valdez 
+
+#include "Procesos.h"
+#include <cstdio>
+#include <sstream>
+#include <algorithm>
+#include <stdexcept>
+
+Proceso* obtenerProcesos(int &total) {
+    FILE* fp = popen("tasklist /FO CSV /NH", "r");
+    if (!fp) throw std::runtime_error("Error obteniendo procesos");
+
+    Proceso* lista = new Proceso[500];
+    total = 0;
+    char linea[1024];
+
+    while (fgets(linea, sizeof(linea), fp) && total < 500) {
+        std::string s(linea);
+        std::stringstream ss(s);
+
+        std::string nombre, pidStr, sesion, numSesion, memoria;
+
+        getline(ss, nombre, ',');
+        getline(ss, pidStr, ',');
+        getline(ss, sesion, ',');
+        getline(ss, numSesion, ',');
+        getline(ss, memoria, ',');
+
+        nombre.erase(remove(nombre.begin(), nombre.end(), '"'), nombre.end());
+        pidStr.erase(remove(pidStr.begin(), pidStr.end(), '"'), pidStr.end());
+        memoria.erase(remove(memoria.begin(), memoria.end(), '"'), memoria.end());
+
+        lista[total].nombre = nombre;
+        lista[total].pid = stoi(pidStr);
+        lista[total].memoria = memoria;
+
+        total++;
+    }
+
+    pclose(fp);
+    return lista;
+}
---
+++ Procesos.h
@@ -0,0 +1,15 @@
+//Alejandra Itzel Ojeda Valdez 
+#ifndef PROCESOS_H
+#define PROCESOS_H
+
+#include <string>
+
+struct Proceso {
+    int pid;
+    std::string nombre;
+    std::string memoria;
+};
+
+Proceso* obtenerProcesos(int &total);
+
+#endif
